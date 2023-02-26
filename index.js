825074b5d25273c8d643ed3cbf3040cc0a0358cc
João Guilherme Ponciano <joaoggponciano@gmail.com>
2023-02-26 14:44:52 -0300

    new ideas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function toggleImage() {
     const imageElement = document.getElementById("acai-img");
-    const images = ["imgs/acai_01.png", "imgs/acai_02.png", "imgs/acai_03.png", "imgs/acai_05.png", "imgs/acai_06.png", "imgs/acai_07.png", "imgs/acai_08.png", "imgs/acai_09.png",];
+    const images = ["imgs/acai_01.png", "imgs/acai_02.png", "imgs/acai_03.png", "imgs/acai_05.png", "imgs/acai_06.png", "imgs/acai_07.png", "imgs/acai_08.png", "imgs/acai_09.png"];
     let currentIndex = 0;
 
     setInterval(function() {
